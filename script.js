b03c3581f4c6c50746f4de63dd7fffff82962c3f
PortugalAndre <depaula_andrep@hotmail.com>
2022-09-14 21:50:42 -0300

    inclusão de escutadores, funções e API DOM

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-function start() {}
+//Função responsável por iniciar o funcionamento de todo o sistema ao receber inputs do HTML
+function start() {
+  var botaoCalcular = document.querySelector('#botaoCalcular');
+  /*MODIFICAR BOTÃO ATRAVÉS DO JS
+  buttonCalculateImc.textContent = 'modifiquei';
+  */
+  botaoCalcular.addEventListener('click', inputRecebido);
+
+  var inputGasolina = document.querySelector('#inputGasolina');
+  var inputEtanol = document.querySelector('#inputEtanol');
+
+  inputGasolina.addEventListener('input', inputRecebido);
+  inputEtanol.addEventListener('input', inputRecebido);
+}
+
+function calular(gasolina, etanol) {
+  return gasolina / etanol;
+}
+
+function inputRecebido() {
+  var inputGasolina = document.querySelector('#inputGasolina');
+  var inputEtanol = document.querySelector('#inputEtanol');
+
+  var gasolina = Number(inputGasolina.value);
+  var etanol = Number(inputEtanol.value);
+
+  resultado = calular(gasolina, etanol);
+
+  resultadoCalculado.textContent = resultado;
+}
 start();
